3cb670738e34aebdf6c010fe47a43ce7d6d0ea24
Sergey Papushin <spapushin@gmail.com>
2018-01-19 18:17:44 -0600

    Add comments to describe asserts in tests

--- src/time/timer/tests.rs
+++ src/time/timer/tests.rs
@@ -39,16 +39,21 @@ fn monotically_increasing_split_times_after_resetting() {
 
     let run = timer.into_run(true);
 
+    // The first segment's time should be unchanged.
     assert_eq!(
         run.segment(0).personal_best_split_time().game_time,
         Some(first)
     );
 
+    // 15.0 is larger than 5.0, so the second segment's time should be
+    // unchanged.
     assert_eq!(
         run.segment(1).personal_best_split_time().game_time,
         Some(second)
     );
 
+    // 10.0 is less than 15.0, and since we want split times to be monotonically
+    // increasing, the third segment's time should be adjusted to 15.0.
     assert_eq!(
         run.segment(2).personal_best_split_time().game_time,
         Some(second)
@@ -82,6 +87,8 @@ fn deleting_best_segment_time_clears_segment_history() {
     editor.select_only(1);
     editor.active_segment().set_split_time(None);
 
+    // Clearing the second segment's split time should not affect the second
+    // segment's best segment time.
     assert_eq!(
         editor.run().segment(1).best_segment_time().game_time,
         Some(second - first)
@@ -89,6 +96,9 @@ fn deleting_best_segment_time_clears_segment_history() {
 
     editor.active_segment().set_best_segment_time(None);
 
+    // Since the second segment's split time is already cleared, clearing its
+    // best segment time should clear the segment history and leave the best
+    // segment time as blank.
     assert_eq!(
         editor.run().segment(1).segment_history().iter().next(),
         None
@@ -103,10 +113,14 @@ fn deleting_best_segment_time_clears_segment_history() {
     editor.select_only(1);
     editor.active_segment().set_best_segment_time(None);
 
+    // Clearing the second segment's best segment time without clearing its
+    // split time should still clear its segment history.
     assert_eq!(
         editor.run().segment(1).segment_history().iter().next(),
         None
     );
+    // Since the second segment's split time was not cleared, the best segment
+    // time should be fixed based on the personal best split times.
     assert_eq!(
         editor.run().segment(1).best_segment_time().game_time,
         Some(second - first)
@@ -140,6 +154,8 @@ fn modifying_best_segment_time_fixes_segment_history() {
     editor.select_only(1);
     editor.active_segment().set_split_time(None);
 
+    // Clearing the second segment's split time should not affect the second
+    // segment's best segment time.
     assert_eq!(
         editor.run().segment(1).best_segment_time().game_time,
         Some(second - first)
@@ -149,6 +165,9 @@ fn modifying_best_segment_time_fixes_segment_history() {
 
     editor.active_segment().set_best_segment_time(new_best);
 
+    // Changing the second segment's best segment time from 5.0 to 7.0 after
+    // clearing the split time should change all times in the segment history so
+    // that none are shorter than 7.0.
     assert_eq!(
         editor
             .run()
@@ -159,6 +178,7 @@ fn modifying_best_segment_time_fixes_segment_history() {
             .and_then(|&(_, t)| t.game_time),
         new_best
     );
+    // The second segment's best segment time should have changed to 7.0.
     assert_eq!(
         editor.run().segment(1).best_segment_time().game_time,
         new_best
@@ -172,6 +192,9 @@ fn modifying_best_segment_time_fixes_segment_history() {
     editor.select_only(1);
     editor.active_segment().set_best_segment_time(new_best);
 
+    // Changing the second segment's best segment time from 5.0 to 7.0 without
+    // clearing the split time first should keep the segment history unaffected.
+    // This is because the PB segment is still 5.0.
     assert_eq!(
         editor
             .run()
@@ -182,6 +205,8 @@ fn modifying_best_segment_time_fixes_segment_history() {
             .and_then(|&(_, t)| t.game_time),
         Some(second - first)
     );
+    // The second segment's best segment time should also be unaffected. This is
+    // because the PB segment is still 5.0.
     assert_eq!(
         editor.run().segment(1).best_segment_time().game_time,
         Some(second - first)
@@ -228,6 +253,9 @@ fn import_pb_into_segment_history() {
 
     let run = timer.run();
 
+    // The previous PB's first segment should be imported into the segment
+    // history with a non-positive index. A non-positive index means that it was
+    // not done during an actual run.
     assert_eq!(
         run.segment(0)
             .segment_history()
@@ -235,6 +263,9 @@ fn import_pb_into_segment_history() {
             .and_then(|t| t.game_time),
         fake_first
     );
+    // The new run's first segment should be imported into the segment history
+    // with a positive index. A positive index means that it was done during an
+    // actual run.
     assert_eq!(
         run.segment(0)
             .segment_history()
@@ -243,7 +274,12 @@ fn import_pb_into_segment_history() {
         Some(real_first)
     );
 
+    // The previous PB's second segment should not be imported into the segment
+    // history. This is because it is a duplicate of the new run's second
+    // segment.
     assert_eq!(run.segment(1).segment_history().get(-1), None);
+    // The new run's second segment should be imported into the segment history
+    // with a positive index.
     assert_eq!(
         run.segment(1)
             .segment_history()
@@ -252,6 +288,8 @@ fn import_pb_into_segment_history() {
         Some(real_second - real_first)
     );
 
+    // The previous PB's third segment should be imported into the segment
+    // history with a non-positive index.
     assert_eq!(
         run.segment(2)
             .segment_history()
@@ -259,6 +297,8 @@ fn import_pb_into_segment_history() {
             .and_then(|t| t.game_time),
         catch! { fake_third? - fake_second? }
     );
+    // The new run's third segment should be imported into the segment history
+    // with a positive index.
     assert_eq!(
         run.segment(2)
             .segment_history()
@@ -302,6 +342,9 @@ fn import_pb_into_segment_history_and_remove_null_values() {
 
     let run = timer.run();
 
+    // The previous PB's first segment should be imported into the segment
+    // history with a non-positive index. A non-positive index means that it was
+    // not done during an actual run.
     assert_eq!(
         run.segment(0)
             .segment_history()
@@ -309,6 +352,9 @@ fn import_pb_into_segment_history_and_remove_null_values() {
             .and_then(|t| t.game_time),
         fake_first
     );
+    // The new run's first segment should be imported into the segment history
+    // with a positive index. A positive index means that it was done during an
+    // actual run.
     assert_eq!(
         run.segment(0)
             .segment_history()
@@ -317,13 +363,26 @@ fn import_pb_into_segment_history_and_remove_null_values() {
         Some(real_first)
     );
 
+    // The previous PB's second segment should not be imported into the segment
+    // history. The second segment's split time was blank for the previous PB,
+    // so it is actually a part of a combined segment with the third segment.
+    // Since the third segment is a duplicate of the new run's third segment,
+    // neither the second nor the third segment should be imported.
     assert_eq!(run.segment(1).segment_history().get(-1), None);
+    // The new run's second segment has a blank split time, so a null time
+    // should be imported into the segment history with a positive index. This
+    // indicates that it is a part of a combined segment with the third segment.
     assert_eq!(
         run.segment(1).segment_history().get(1).map(|t| t.game_time),
         Some(None)
     );
 
+    // The previous PB's third segment should not be imported into the segment
+    // history. This is because the third segment is a duplicate of the new
+    // run's third segment.
     assert_eq!(run.segment(2).segment_history().get(-1), None);
+    // The new run's third segment should be imported into the segment history
+    // with a positive index.
     assert_eq!(
         run.segment(2)
             .segment_history()
@@ -353,11 +412,21 @@ fn import_best_segment_with_game_time_usage() {
     editor.insert_segment_above();
 
     let history = editor.run().segment(0).segment_history();
+    // The newly inserted segment's history should have a null time with a
+    // non-positive index. This represents a skipped split for the imported best
+    // segment with a time of 4.0.
     assert_eq!(history.get(0).map(|t| t.game_time), Some(None));
+    // The newly inserted segment's history should have a null time with a
+    // positive index. This represents a skipped split for an actual run with a
+    // time of 5.0.
     assert_eq!(history.get(1).map(|t| t.game_time), Some(None));
 
     let history = editor.run().segment(1).segment_history();
+    // The second segment's history should have a time of 4.0 with a
+    // non-positive index, which represents the imported best segment.
     assert_eq!(history.get(0).and_then(|t| t.game_time), best);
+    // The second segment's history should have a time of 5.0 with a positive
+    // index, which represents an actual run.
     assert_eq!(history.get(1).and_then(|t| t.game_time), Some(first));
 }
 
