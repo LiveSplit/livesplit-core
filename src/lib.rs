1702b390db625d6fe0f52bace2d294800cc2ffa6
Christopher Serr <christopher.serr@gmail.com>
2018-04-16 22:39:04 +0200

    Don't use uninitialized memory for thread locals
    That's just asking for Undefined Behavior, which due to niche filling
    actually seems to have happened. We can easily initialize these with
    cheap default values.
    
    Fixes #122

--- src/lib.rs
+++ src/lib.rs
@@ -69,18 +69,17 @@ macro_rules! catch {
     }
 }
 
-mod hotkey_config;
-mod hotkey_system;
-mod image;
 pub mod analysis;
 pub mod comparison;
 pub mod component;
+mod hotkey_config;
+mod hotkey_system;
+mod image;
 pub mod layout;
 pub mod run;
 pub mod settings;
 pub mod time;
 
-pub use chrono::{DateTime, Utc};
 pub use self::hotkey_config::HotkeyConfig;
 pub use self::hotkey_system::HotkeySystem;
 pub use self::image::Image;
@@ -89,3 +88,4 @@ pub use self::layout::{Component, Editor as LayoutEditor,
 pub use self::run::{Attempt, Editor as RunEditor, Run, RunMetadata, Segment, SegmentHistory};
 pub use self::time::{AtomicDateTime, GameTime, RealTime, SharedTimer, Time, TimeSpan, TimeStamp,
                      Timer, TimerPhase, TimingMethod};
+pub use chrono::{DateTime, Utc};
