cfa5ce7a219d585acc7f962d24fcb9993e358770
Christopher Serr <christopher.serr@gmail.com>
2019-05-15 18:08:28 +0200

    Reexport components in component module

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -19,6 +19,22 @@ pub mod timer;
 pub mod title;
 pub mod total_playtime;
 
+pub use blank_space::Component as BlankSpace;
+pub use current_comparison::Component as CurrentComparison;
+pub use current_pace::Component as CurrentPace;
+pub use delta::Component as Delta;
+pub use detailed_timer::Component as DetailedTimer;
+pub use graph::Component as Graph;
+pub use possible_time_save::Component as PossibleTimeSave;
+pub use previous_segment::Component as PreviousSegment;
+pub use separator::Component as Separator;
+pub use splits::Component as Splits;
+pub use sum_of_best::Component as SumOfBest;
+pub use text::Component as Text;
+pub use timer::Component as Timer;
+pub use title::Component as Title;
+pub use total_playtime::Component as TotalPlaytime;
+
 use crate::settings::{Color, Gradient};
 use palette::rgb::Rgb;
 use palette::Alpha;
