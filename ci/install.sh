e9ab0b178d2353d6df71dc930e1c2cdca40ea3b2
Christopher Serr <christopher.serr@gmail.com>
2019-09-09 14:25:44 +0200

    Fix cross by staying on v0.1.14 for now
    cc #237

--- ci/install.sh
+++ ci/install.sh
@@ -65,11 +65,15 @@ main() {
     esac
 
     # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0.1.0-9.]+$' \
-                       | $sort --version-sort \
-                       | tail -n1)
+    # local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    #                    | cut -d/ -f3 \
+    #                    | grep -E '^v[0.1.0-9.]+$' \
+    #                    | $sort --version-sort \
+    #                    | tail -n1)
+
+    # FIXME: We are staying on 0.1.14 until cross works again for the targets that we compile to.
+    # https://github.com/LiveSplit/livesplit-core/issues/237
+    local tag=v0.1.14
 
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
