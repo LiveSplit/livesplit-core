09cbfb4b2832dc7f0b8d4896aa60ac49581f8cd0
Christopher Serr <christopher.serr@gmail.com>
2019-09-18 13:59:42 +0200

    Resolve most remaining CI issues

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -11,21 +11,16 @@ main() {
     fi
 
     # This fetches latest stable release
-    # local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-    #                    | cut -d/ -f3 \
-    #                    | grep -E '^v[0.1.0-9.]+$' \
-    #                    | $sort --version-sort \
-    #                    | tail -n1)
-
-    # FIXME: We use a custom pre-release version that works with GitHub Actions.
-    # Also don't forget to also change back from CryZe/cross down there.
-    # https://github.com/LiveSplit/livesplit-core/issues/237
-    local tag=v0.1.16-pre
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/rust-embedded/cross \
+                       | cut -d/ -f3 \
+                       | grep -E '^v[0.1.0-9.]+$' \
+                       | $sort --version-sort \
+                       | tail -n1)
 
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
-           --git CryZe/cross \
+           --git rust-embedded/cross \
            --tag $tag \
            --target $target
 }
