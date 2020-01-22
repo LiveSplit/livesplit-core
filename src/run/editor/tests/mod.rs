1b52a7f73f93d97f0aec096d534fd733dcd0515c
Dark <dark@volatile.bz>
2020-01-22 18:39:53 -0500

    Run Fixing Algorithms when opening splits
    closes #21

--- src/run/editor/tests/mod.rs
+++ src/run/editor/tests/mod.rs
@@ -1,4 +1,5 @@
 use super::Editor;
+use crate::tests_helper::{create_timer, run_with_splits};
 use crate::{Run, Segment};
 
 mod comparison;
@@ -86,4 +87,27 @@ fn select_additionally_oob() {
     let mut editor = Editor::new(run).unwrap();
 
     editor.select_additionally(1);
+}
+
+#[test]
+fn fix_run_upon_creation() {
+    let mut timer = create_timer(&["A", "B"]);
+    run_with_splits(&mut timer, &[3.0, 6.0]);
+    run_with_splits(&mut timer, &[2.0, 4.0]);
+    let mut run = timer.into_run(true);
+
+    // We pop the last attempt from the history, but keep it in the segment
+    // history, which makes the segment history elements unattached.
+    run.attempt_history.pop().unwrap();
+
+    // This should cause the run to be fixed.
+    let run = Editor::new(run).unwrap().close();
+
+    let segments = run.segments();
+
+    assert_eq!(segments[0].segment_history().try_get_min_index(), Some(0));
+    assert_eq!(segments[0].segment_history().try_get_max_index(), Some(1));
+
+    assert_eq!(segments[1].segment_history().try_get_min_index(), Some(0));
+    assert_eq!(segments[1].segment_history().try_get_max_index(), Some(1));
 }
--- src/run/tests/fixing.rs
+++ src/run/tests/fixing.rs
@@ -1,4 +1,5 @@
 use crate::tests_helper::{create_timer, run_with_splits};
+use crate::Timer;
 
 #[test]
 fn reattaches_unattached_segment_history_elements_by_using_negative_ids() {
@@ -12,6 +13,57 @@ fn reattaches_unattached_segment_history_elements_by_using_negative_ids() {
     run.attempt_history.pop().unwrap();
 
     run.fix_splits();
+
+    let segments = run.segments();
+
+    assert_eq!(segments[0].segment_history().try_get_min_index(), Some(0));
+    assert_eq!(segments[0].segment_history().try_get_max_index(), Some(1));
+
+    assert_eq!(segments[1].segment_history().try_get_min_index(), Some(0));
+    assert_eq!(segments[1].segment_history().try_get_max_index(), Some(1));
+}
+
+// The below tests should be in crate::timing::timer::tests, but we ended up
+// having to put them here due to run.attempt_history being private.
+
+#[test]
+fn timer_fix_run_upon_creation() {
+    let mut timer = create_timer(&["A", "B"]);
+    run_with_splits(&mut timer, &[3.0, 6.0]);
+    run_with_splits(&mut timer, &[2.0, 4.0]);
+    let mut run = timer.into_run(true);
+
+    // We pop the last attempt from the history, but keep it in the segment
+    // history, which makes the segment history elements unattached.
+    run.attempt_history.pop().unwrap();
+
+    // This should cause the run to be fixed.
+    let run = Timer::new(run).unwrap().into_run(true);
+
+    let segments = run.segments();
+
+    assert_eq!(segments[0].segment_history().try_get_min_index(), Some(0));
+    assert_eq!(segments[0].segment_history().try_get_max_index(), Some(1));
+
+    assert_eq!(segments[1].segment_history().try_get_min_index(), Some(0));
+    assert_eq!(segments[1].segment_history().try_get_max_index(), Some(1));
+}
+
+#[test]
+fn timer_fix_run_upon_replacement() {
+    let mut timer = create_timer(&["A", "B"]);
+    run_with_splits(&mut timer, &[3.0, 6.0]);
+    run_with_splits(&mut timer, &[2.0, 4.0]);
+    let mut run = timer.into_run(true);
+
+    // We pop the last attempt from the history, but keep it in the segment
+    // history, which makes the segment history elements unattached.
+    run.attempt_history.pop().unwrap();
+
+    // This should cause the run to be fixed.
+    let mut timer = create_timer(&["A", "B"]);
+    timer.set_run(run).unwrap();
+    let run = timer.into_run(true);
 
     let segments = run.segments();
 
