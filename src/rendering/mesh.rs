ffd7410d4b6e3dae906ef1dd96554cda6aea54bc
Christopher Serr <christopher.serr@gmail.com>
2020-10-31 23:16:41 +0100

    Allow accessing the mesh buffers as byte slices
    When building a backend for the renderer you likely will end up needing
    to upload the vertex and index buffers of the meshes to the GPU. Most
    APIs want to consume byte slices for that. So this adds some convenience
    methods that allow accessing the vertices and the indices as byte
    slices.

--- src/rendering/mesh.rs
+++ src/rendering/mesh.rs
@@ -9,7 +9,7 @@ use lyon::tessellation::{
 /// The vertex types describes a single point of a mesh used to form triangles.
 /// It uses a C compatible layout such that it can be directly uploaded to a GPU.
 #[repr(C)]
-#[derive(Copy, Clone, PartialOrd, PartialEq)]
+#[derive(Copy, Clone, PartialOrd, PartialEq, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct Vertex {
     /// The x coordinate of the vertex.
     pub x: f32,
@@ -53,6 +53,21 @@ impl Mesh {
     /// chunk of three indices pointing into the `vertices` makes up a triangle.
     pub fn indices(&self) -> &[u16] {
         &self.buffers.indices
+    }
+
+    /// The vertices that make up the mesh. This method returns the vertices as
+    /// a slice of bytes, which may be easier to consume for various graphics
+    /// APIs.
+    pub fn vertices_as_bytes(&self) -> &[u8] {
+        bytemuck::cast_slice(self.vertices())
+    }
+
+    /// The indices describe the actual triangles that make up the mesh. Each
+    /// chunk of three indices pointing into the `vertices` makes up a triangle.
+    /// This method returns the indices as a slice of bytes, which may be easier
+    /// to consume for various graphics APIs.
+    pub fn indices_as_bytes(&self) -> &[u8] {
+        bytemuck::cast_slice(self.indices())
     }
 }
 
