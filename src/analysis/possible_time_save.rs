a1ee0e1b08259397565e9a02a0e8392f7b63bd91
KaDiWa4 <kalle.wachsmuth@gmail.com>
2022-01-02 20:18:20 +0100

    Make `updates_frequently` more consistent

--- src/analysis/possible_time_save.rs
+++ src/analysis/possible_time_save.rs
@@ -14,56 +14,56 @@ use crate::{analysis, timing::Snapshot, TimeSpan};
 /// current attempt is used if it gets longer than the segment time of the
 /// segment the possible time save is calculated for. So the possible time save
 /// shrinks towards zero as time goes on. The time returned by this function can
-/// never be below zero.
+/// never be below zero. Additionally a boolean is returned that indicates if
+/// the value is currently actively changing as time is being lost.
 pub fn calculate(
     timer: &Snapshot<'_>,
     segment_index: usize,
     comparison: &str,
     live: bool,
-) -> Option<TimeSpan> {
+) -> (Option<TimeSpan>, bool) {
     let segments = timer.run().segments();
     let method = timer.current_timing_method();
     let mut prev_time = TimeSpan::zero();
     let segment = timer.run().segment(segment_index);
-    let mut best_segments = segment.best_segment_time()[method];
 
-    for segment in segments[..segment_index].iter().rev() {
-        if let Some(best_segments) = &mut best_segments {
+    catch! {
+        let mut best_segments = segment.best_segment_time()[method]?;
+
+        for segment in segments[..segment_index].iter().rev() {
             if let Some(split_time) = segment.comparison(comparison)[method] {
                 prev_time = split_time;
                 break;
             } else if let Some(best_segment) = segment.best_segment_time()[method] {
-                *best_segments += best_segment;
+                best_segments += best_segment;
             }
-        } else {
-            break;
         }
-    }
 
-    catch! {
-        let mut time = segment.comparison(comparison)[method]? - best_segments? - prev_time;
+        let mut time = segment.comparison(comparison)[method]? - best_segments - prev_time;
+        let mut updates_frequently = false;
 
-        catch! {
-            if live && timer.current_split_index()? == segment_index {
-                let segment_delta = analysis::live_segment_delta(
-                    timer,
-                    segment_index,
-                    comparison,
-                    method,
-                )?;
+        if live && timer.current_split_index() == Some(segment_index) {
+            if let Some(segment_delta) = analysis::live_segment_delta(
+                timer,
+                segment_index,
+                comparison,
+                method,
+            ) {
                 let segment_delta = TimeSpan::zero() - segment_delta;
                 if segment_delta < time {
                     time = segment_delta;
+                    updates_frequently = timer.current_phase().is_running();
                 }
-            };
-        };
+            }
+        }
 
         if time < TimeSpan::zero() {
-            TimeSpan::zero()
+            (Some(TimeSpan::zero()), false)
         } else {
-            time
+            (Some(time), updates_frequently)
         }
     }
+    .unwrap_or_default()
 }
 
 /// Calculates how much time could be saved on the remainder of the run with the
@@ -73,14 +73,23 @@ pub fn calculate(
 /// actually be saved. This information is always live, so the total possible
 /// time save will shrink towards zero throughout the run and when time is lost
 /// on a segment. The time returned by this function can never be below zero.
-pub fn calculate_total(timer: &Snapshot<'_>, segment_index: usize, comparison: &str) -> TimeSpan {
+/// Additionally a boolean is returned that indicates if the value is currently
+/// actively changing as time is being lost.
+pub fn calculate_total(
+    timer: &Snapshot<'_>,
+    segment_index: usize,
+    comparison: &str,
+) -> (TimeSpan, bool) {
     let mut total = TimeSpan::zero();
+    let mut updates_frequently = false;
 
     for index in segment_index..timer.run().len() {
-        if let Some(time_save) = calculate(timer, index, comparison, true) {
+        let (time_save, changing) = calculate(timer, index, comparison, true);
+        updates_frequently |= changing;
+        if let Some(time_save) = time_save {
             total += time_save;
         }
     }
 
-    total
+    (total, updates_frequently)
 }
