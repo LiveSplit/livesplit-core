43042834a8d1e16759e35b71f45b7916a9c6ae16
Christopher Serr <christopher.serr@gmail.com>
2023-11-11 00:14:02 +0100

    Add `SettingsList` for storing lists of settings
    This is a new type that is used to store lists of settings in addition
    to `SettingsMap`. The difference is that for lists you don't use keys to
    look up the values, but instead you use indices, or more likely,
    iteration.

--- crates/livesplit-auto-splitting/src/settings.rs
+++ crates/livesplit-auto-splitting/src/settings.rs
@@ -43,8 +43,10 @@ pub enum UserSettingKind {
 #[non_exhaustive]
 #[derive(Clone)]
 pub enum SettingValue {
-    /// A map of settings that are stored in the [`SettingsMap`].
+    /// A map of settings that are stored in a [`SettingsMap`].
     Map(SettingsMap),
+    /// A list of settings that are stored in a [`SettingsList`].
+    List(SettingsList),
     /// A boolean value.
     Bool(bool),
     /// A 64-bit signed integer value.
@@ -59,6 +61,7 @@ impl fmt::Debug for SettingValue {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Map(v) => fmt::Debug::fmt(v, f),
+            Self::List(v) => fmt::Debug::fmt(v, f),
             Self::Bool(v) => fmt::Debug::fmt(v, f),
             Self::I64(v) => fmt::Debug::fmt(v, f),
             Self::F64(v) => fmt::Debug::fmt(v, f),
@@ -77,6 +80,7 @@ pub struct SettingsMap {
 }
 
 impl fmt::Debug for SettingsMap {
+    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(&self.values, f)
     }
@@ -84,6 +88,8 @@ impl fmt::Debug for SettingsMap {
 
 impl SettingsMap {
     /// Creates a new empty settings map.
+    #[must_use]
+    #[inline]
     pub fn new() -> Self {
         Self::default()
     }
@@ -91,6 +97,7 @@ impl SettingsMap {
     /// Sets a setting to the new value. If the key of the setting doesn't exist
     /// yet it will be stored as a new value. Otherwise the value will be
     /// updated.
+    #[inline]
     pub fn insert(&mut self, key: Arc<str>, value: SettingValue) {
         Arc::make_mut(&mut self.values).insert(key, value);
     }
@@ -98,17 +105,145 @@ impl SettingsMap {
     /// Accesses the value of a setting by its key. While the setting may exist
     /// as part of the user settings, it may not have been stored into the
     /// settings map yet, so it may not exist, despite being registered.
+    #[must_use]
+    #[inline]
     pub fn get(&self, key: &str) -> Option<&SettingValue> {
         self.values.get(key)
     }
 
+    /// Accesses the value of a setting by its index. The index is the position
+    /// of the setting in the list of all settings. This may be useful for
+    /// iterating over all settings. Prefer using [`iter`](Self::iter) in most
+    /// situations though.
+    #[must_use]
+    #[inline]
+    pub fn get_by_index(&self, index: usize) -> Option<(&str, &SettingValue)> {
+        self.values.get_index(index).map(|(k, v)| (k.as_ref(), v))
+    }
+
     /// Iterates over all the setting keys and their values in the map.
+    #[inline]
     pub fn iter(&self) -> impl Iterator<Item = (&str, &SettingValue)> {
         self.values.iter().map(|(k, v)| (k.as_ref(), v))
     }
 
+    /// Returns the number of settings that are stored in the map.
+    #[must_use]
     #[inline]
-    pub(super) fn is_unchanged(&self, other: &Self) -> bool {
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    /// Returns [`true`] if the map doesn't contain any settings.
+    #[must_use]
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.values.is_empty()
+    }
+
+    /// Returns [`true`] if the identity of the map is the same as the identity
+    /// of the other map. Maps use the copy-on-write principle. This means that
+    /// cloning a map is cheap because it references all the same data as the
+    /// original until one of the variables is changed. With this function you
+    /// can check if two variables internally share the same data and are
+    /// therefore identical. This is useful to determine if the map has changed
+    /// since the last time it was checked. You may use this as part of a
+    /// compare-and-swap loop.
+    #[must_use]
+    #[inline]
+    pub fn is_unchanged(&self, other: &Self) -> bool {
         Arc::ptr_eq(&self.values, &other.values)
+    }
+}
+
+/// A list of settings that may be used as a [`SettingValue`] as part of a
+/// [`SettingsMap`]. It logically resembles a [`Vec`] of [`SettingValue`] and
+/// therefore provides similar functionality.
+#[derive(Clone, Default)]
+pub struct SettingsList {
+    list: Arc<Vec<SettingValue>>,
+}
+
+impl fmt::Debug for SettingsList {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&self.list, f)
+    }
+}
+
+impl SettingsList {
+    /// Creates a new empty settings list.
+    #[must_use]
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Returns the number of settings that are stored in the list.
+    #[must_use]
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.list.len()
+    }
+
+    /// Returns [`true`] if the list doesn't contain any settings.
+    #[must_use]
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.list.is_empty()
+    }
+
+    /// Accesses the value of a setting by its index. The index is the position
+    /// of the setting in the list of all settings. This may be useful for
+    /// iterating over all settings. Prefer using [`iter`](Self::iter) in most
+    /// situations though.
+    #[must_use]
+    #[inline]
+    pub fn get(&self, index: usize) -> Option<&SettingValue> {
+        self.list.get(index)
+    }
+
+    /// Pushes a setting value to the end of the list.
+    #[inline]
+    pub fn push(&mut self, value: SettingValue) {
+        Arc::make_mut(&mut self.list).push(value);
+    }
+
+    /// Inserts a setting value at the given index. If the index is larger than
+    /// the length of the list, the value will be appended to the end of the
+    /// list.
+    #[inline]
+    pub fn insert(&mut self, index: usize, value: SettingValue) {
+        let list = Arc::make_mut(&mut self.list);
+        list.insert(index.min(list.len()), value);
+    }
+
+    /// Removes the setting value at the given index and returns it. If the
+    /// index is larger than the length of the list, [`None`] will be returned.
+    #[inline]
+    pub fn remove(&mut self, index: usize) -> Option<SettingValue> {
+        let list = Arc::make_mut(&mut self.list);
+        if index >= list.len() {
+            return None;
+        }
+        Some(list.remove(index))
+    }
+
+    /// Iterates over all the setting values in the list.
+    #[inline]
+    pub fn iter(&self) -> std::slice::Iter<'_, SettingValue> {
+        self.list.iter()
+    }
+
+    /// Returns [`true`] if the identity of the list is the same as the identity
+    /// of the other list. Lists use the copy-on-write principle. This means
+    /// that cloning a list is cheap because it references all the same data as
+    /// the original until one of the variables is changed. With this function
+    /// you can check if two variables internally share the same data and are
+    /// therefore identical. This is useful to determine if the list has changed
+    /// since the last time it was checked. You may use this as part of a
+    /// compare-and-swap loop.
+    pub fn is_unchanged(&self, other: &Self) -> bool {
+        Arc::ptr_eq(&self.list, &other.list)
     }
 }
