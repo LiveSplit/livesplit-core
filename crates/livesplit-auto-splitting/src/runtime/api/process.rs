98e38262583a0297dda8290be3462bcf0285a87d
Jujstme <just.tribe@gmail.com>
2024-01-02 21:13:06 +0100

    Allowing the runtime to correctly hook into long-named Windows processes on Linux (Proton/Wine)

--- crates/livesplit-auto-splitting/src/runtime/api/process.rs
+++ crates/livesplit-auto-splitting/src/runtime/api/process.rs
@@ -16,7 +16,11 @@ pub fn bind<T: Timer>(linker: &mut Linker<Context<T>>) -> Result<(), CreationErr
         .func_wrap("env", "process_attach", {
             |mut caller: Caller<'_, Context<T>>, ptr: u32, len: u32| {
                 let (memory, context) = memory_and_context(&mut caller);
-                let process_name = get_str(memory, ptr, len)?;
+                let mut process_name = get_str(memory, ptr, len)?;
+                #[cfg(target_os = "linux")]
+                if process_name.ends_with(".exe") {
+                    process_name = &process_name[..15.min(len as _)];
+                }
                 Ok(
                     if let Ok(p) = Process::with_name(process_name, &mut context.process_list) {
                         context.timer.log(format_args!(
