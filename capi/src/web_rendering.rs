5de5733f184bb9eb2a25c6d25cb624fdad7d3124
Christopher Serr <christopher.serr@gmail.com>
2024-04-27 17:37:37 +0200

    Add resize hint to the Web Renderer
    The web renderer now returns the new size whenever the element is
    supposed to be resized. This is important so adding or removing a
    component to the layout or loading a new layout will adjust the size of
    the element accordingly. This is the same way the other renderers handle
    it, so we should do the same for the web renderer.
    
    Additionally this fixes a bug in the way the strings got passed from
    JavaScript to Rust, where we introduced a difference between the length
    of a string and the capacity of its allocation, but didn't properly
    differentiate between them everywhere.

--- capi/src/web_rendering.rs
+++ capi/src/web_rendering.rs
@@ -8,12 +8,12 @@ use web_sys::Element;
 /// The web renderer renders into a canvas element. The element can then be
 /// attached anywhere in the DOM with any desired positioning and size.
 #[wasm_bindgen]
-pub struct CanvasRenderer {
+pub struct WebRenderer {
     inner: web::Renderer,
 }
 
 #[wasm_bindgen]
-impl CanvasRenderer {
+impl WebRenderer {
     /// Creates a new web renderer that renders into a canvas element. The
     /// element can then be attached anywhere in the DOM with any desired
     /// positioning and size. There are two CSS fonts that are used as the
@@ -21,7 +21,7 @@ impl CanvasRenderer {
     /// fully loaded before creating the renderer as otherwise information about
     /// a fallback font is cached instead.
     #[allow(clippy::new_without_default)]
-    pub fn new() -> CanvasRenderer {
+    pub fn new() -> Self {
         Self {
             inner: web::Renderer::new(),
         }
@@ -35,9 +35,11 @@ impl CanvasRenderer {
 
     /// Renders the layout state into the canvas. The image cache is used to
     /// retrieve images that are used in the layout state.
-    pub unsafe fn render(&mut self, state: usize, image_cache: usize) {
-        let state = unsafe { core::mem::transmute::<usize, &LayoutState>(state) };
-        let image_cache = unsafe { core::mem::transmute::<usize, &ImageCache>(image_cache) };
-        self.inner.render(state, image_cache);
+    pub unsafe fn render(
+        &mut self,
+        state: *const LayoutState,
+        image_cache: *const ImageCache,
+    ) -> Option<Box<[f32]>> {
+        self.inner.render(&*state, &*image_cache).map(Box::from)
     }
 }
